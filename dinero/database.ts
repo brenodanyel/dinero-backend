74976f7f9268569e4507d4f7bca639e6bb572490
bdls <bdls1704@hotmail.com>
2025-01-12 23:10:16 -0300

    chore(database): format code and improve readability

--- dinero/database.ts
+++ dinero/database.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from '@prisma/client';
-import { SQLDatabase } from 'encore.dev/storage/sqldb';
+import { PrismaClient } from '@prisma/client'
+import { SQLDatabase } from 'encore.dev/storage/sqldb'
 
 const DB = new SQLDatabase('dinero', {
-	migrations: {
-		path: './prisma/migrations',
-		source: 'prisma',
-	},
-});
+  migrations: {
+    path: './prisma/migrations',
+    source: 'prisma',
+  },
+})
 
 // Setup prisma client with connection string
 export const prisma = new PrismaClient({
-	datasources: {
-		db: {
-			url: DB.connectionString,
-		},
-	},
-});
+  datasources: {
+    db: {
+      url: DB.connectionString,
+    },
+  },
+})
